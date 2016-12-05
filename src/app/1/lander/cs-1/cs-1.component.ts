024cfe9c4e4dfcdca82cdd35315f3bc4bb0c2b60
John Bentley <jwbentle@gmail.com>
2016-12-05 15:21:25 -0800

    Updated CS1 component variables

--- src/app/1/lander/cs-1/cs-1.component.ts
+++ src/app/1/lander/cs-1/cs-1.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cs-1.component.scss']
 })
 export class Cs1Component implements OnInit {
+  sectionOneBulletOne = '100% potent L-Arginine enhancement';
+  sectionOneBulletTwo = 'May increase blood flow and improve muscular tone';
+  sectionOneBulletThree = 'May help achieve ideal physique';
 
   constructor() { }
 
