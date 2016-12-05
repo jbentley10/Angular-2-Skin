41e8a7bd30376ac3dde84bed4dd9d6d22582e207
Traivs Rea <travis@travisrea.com>
2016-12-05 15:02:16 -0800

    trying to re-upload

---
+++ src/app/1/checkout/check-form/check-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-form',
+  templateUrl: './check-form.component.html',
+  styleUrls: ['./check-form.component.scss']
+})
+export class CheckFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
