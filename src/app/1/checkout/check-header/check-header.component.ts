76f067e8920d48784616c7335ae76372963ffd38
John Bentley <jwbentle@gmail.com>
2016-12-05 16:02:48 -0800

    Added check header component

---
+++ src/app/1/checkout/check-header/check-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-header',
+  templateUrl: './check-header.component.html',
+  styleUrls: ['./check-header.component.scss']
+})
+export class CheckHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
