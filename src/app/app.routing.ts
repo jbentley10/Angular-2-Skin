884bf459e537db1a6f2247a05bdde8b8fe4d7399
John Bentley <jwbentle@gmail.com>
2016-12-06 10:14:25 -0800

    Updated routing to include terms and the lander page. Terms and Conditions link in footer works now

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { LanderComponent } from './1/lander/lander.component';
 import { CheckoutComponent } from './1/checkout/checkout.component';
 import { ThankyouComponent } from './1/thankyou/thankyou.component';
+import { TermsComponent } from './1/terms/terms.component';
 
 const APP_ROUTES: Routes = [
-    { path: 'checkout', component: CheckoutComponent },
-      { path: 'thankyou', component: ThankyouComponent },
-  { path: '', component: LanderComponent },
-
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'thankyou', component: ThankyouComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'lander', component: LanderComponent },
+  { path: '', component: LanderComponent }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
