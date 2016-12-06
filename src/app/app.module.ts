e0f766505966bcbfed3e1dc8b7819bfa2ce75c5b
John Bentley <jwbentle@gmail.com>
2016-12-06 10:07:33 -0800

    Added comments to app.module.ts, reorganized component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+// Landing Page
 import { LanderComponent } from './1/lander/lander.component';
-import { CheckoutComponent } from './1/checkout/checkout.component';
-import { ThankyouComponent } from './1/thankyou/thankyou.component';
-import { FooterComponent } from './1/footer/footer.component';
 import { MainInfoComponent } from './1/lander/main-info/main-info.component';
 import { MainFormComponent } from './1/lander/main-form/main-form.component';
 import { Cs1Component } from './1/lander/cs-1/cs-1.component';
 import { Cs2Component } from './1/lander/cs-2/cs-2.component';
+
+// Footer
+import { FooterComponent } from './1/footer/footer.component';
+
+// Checkout Page
+import { CheckoutComponent } from './1/checkout/checkout.component';
+import { CheckHeaderComponent } from './1/checkout/check-header/check-header.component';
 import { CheckFormComponent } from './1/checkout/check-form/check-form.component';
 import { CheckMainComponent } from './1/checkout/check-main/check-main.component';
+
+// Thank You Page
 import { ThankMainComponent } from './1/thankyou/thank-main/thank-main.component';
+import { ThankyouComponent } from './1/thankyou/thankyou.component';
+
+// Terms Page
+import { TermsComponent } from './1/terms/terms.component';
 
 import { routing } from './app.routing';
-
-import { CheckHeaderComponent } from './1/checkout/check-header/check-header.component';
 import { GlobalDirective } from './global.directive';
-import { TermsComponent } from './1/terms/terms.component';
 
 @NgModule({
   declarations: [
