c4978fd8cae945fa69a2c5afaa51de46375b605a
Traivs Rea <travis@travisrea.com>
2016-12-08 13:34:07 -0800

    form hell pt1

--- src/app/1/lander/main-form/main-form.component.ts
+++ src/app/1/lander/main-form/main-form.component.ts
@@ -1,37 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable} from '@angular/core';
 import { GlobalDirective } from '../../../global.directive';
 import { NgForm } from "@angular/forms";
+
+
+
 
 @Component({
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
-  styleUrls: ['./main-form.component.scss']
-  // template: `
-  // <div class="ui raised segment">
-  //   <h2 class="ui header">Demo Form: Sku</h2>
-  //   <form #f="ngForm"
-  //         (ngSubmit)="onSubmit(f.value)"
-  //         class="ui form">
-  //
-  //     <div class="field">
-  //       <label for="skuInput">SKU</label>
-  //       <input type="text"
-  //              id="skuInput"
-  //              placeholder="SKU"
-  //              name="sku" ngModel>
-  //     </div>
-  //
-  //     <button type="submit" class="ui button">Submit</button>
-  //   </form>
-  // </div>
-  // `
+  styleUrls: ['./main-form.component.scss'],
+  providers: [GlobalDirective]
+
 })
+@Injectable()
 export class MainFormComponent implements OnInit {
-  onSubmit(form: any): void {
+    
+
+  onSubmit(form: any){
     console.log('you submitted value:', form);
+    console.log(form.fName);
+    var fName = form.fName;
+    var lName = form.lName;
+    console.log(fName, lName);
+    return fName;
   }
 
+
   constructor (public echos: GlobalDirective) {}
+
 
   ngOnInit() {
   }
--- src/app/1/thankyou/thankyou.component.ts
+++ src/app/1/thankyou/thankyou.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ThankMainComponent } from './thank-main/thank-main.component';
 import { GlobalDirective } from '../../global.directive';
 
+
 @Component({
   selector: 'app-thankyou',
   templateUrl: './thankyou.component.html',
   styleUrls: ['./thankyou.component.scss']
+
+
 })
 export class ThankyouComponent implements OnInit {
 
-  constructor (public echos: GlobalDirective) {}
+  constructor (public echos: GlobalDirective) {
+
+  }
 
   ngOnInit() {
   }
