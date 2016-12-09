be0c7e49986b6df3983d90ba2855cb22e627540d
jbentley10 <jwbentle@gmail.com>
2016-12-08 16:07:06 -0800

    Switched checkout for component to use form validators

--- src/app/1/checkout/check-form/check-form.component.ts
+++ src/app/1/checkout/check-form/check-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { GlobalDirective } from '../../../global.directive';
-import { NgForm } from "@angular/forms";
+import { NgForm, FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-check-form',
@@ -8,6 +8,27 @@ import { NgForm } from "@angular/forms";
   styleUrls: ['./check-form.component.scss']
 })
 export class CheckFormComponent {
+  checkForm : FormGroup;
+
+  constructor(fb: FormBuilder){
+    this.checkForm = fb.group({
+      'billaddress' : [null, Validators.compose([Validators.required, Validators.minLength(5)])],
+      'billcity': [null, Validators.required],
+      'billstate': [null, Validators.required],
+      'billcountry': [null, Validators.required],
+      'billzip': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5)])],
+      'ccnumber': [null, Validators.compose([Validators.required, Validators.minLength(14), Validators.maxLength(15)])],
+      'cardExpMonth': [null, Validators.required],
+      'cardExpYear': [null, Validators.required],
+      'cvvnumber': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(4)])],
+      'confirm': false
+    })
+  }
+
+  submitForm(value: any){
+    console.log(value);
+  }
+
   public hideBilling = true;
 
   public toggleHiddenBilling() {
--- src/app/1/lander/main-form/main-form.component.ts
+++ src/app/1/lander/main-form/main-form.component.ts
@@ -6,25 +6,6 @@ import { NgForm } from "@angular/forms";
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.scss']
-  // template: `
-  // <div class="ui raised segment">
-  //   <h2 class="ui header">Demo Form: Sku</h2>
-  //   <form #f="ngForm"
-  //         (ngSubmit)="onSubmit(f.value)"
-  //         class="ui form">
-  //
-  //     <div class="field">
-  //       <label for="skuInput">SKU</label>
-  //       <input type="text"
-  //              id="skuInput"
-  //              placeholder="SKU"
-  //              name="sku" ngModel>
-  //     </div>
-  //
-  //     <button type="submit" class="ui button">Submit</button>
-  //   </form>
-  // </div>
-  // `
 })
 export class MainFormComponent implements OnInit {
   onSubmit(form: any): void {
